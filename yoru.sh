73077ea8d456076da31ff35122594003d7afd8f3
cappuccino <rianaga80@gmail.com>
2026-07-16 09:25:25 +0700

    Update yoru.sh

--- yoru.sh
+++ yoru.sh
@@ -25,7 +25,7 @@ KIMG="$OUTDIR/Image.gz"
 export PATH="$ROOTDIR/zyc-clang/bin:$PATH"
 
 # ================= INFO =================
-KERNEL_NAME="Yoru"
+KERNEL_NAME="Mystrile"
 DEVICE="rolex"
 
 # =============== DATE (WIB) ===============
